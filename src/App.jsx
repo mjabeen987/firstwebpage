098af02472b386cc64f6afe1c1e754de2f557f38
mjabeen987 <mahjabeen@accelcq.com>
2025-06-25 11:39:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React + Vite!</h1>
+      <p>Website built in VS Code 💻</p>
+    </div>
+  );
+}
+
+export default App
+
+
+
+
